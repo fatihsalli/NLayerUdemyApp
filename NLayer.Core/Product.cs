e62348ab8f2c506b676db9ccd58fb05fbc738a79
Fatih Şallı <mimsallifatih@gmail.com>
2022-10-16 19:39:20 +0300

    IGenericRepository

---
+++ NLayer.Core/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Core
+{
+    public class Product:BaseEntity
+    {
+        //Constructor tetiklendiğinde name gelmez ise hata fırlatması için aşağıdaki fonksiyon yazılabilir. Name alanının null gelmemesi için oluşturulmuştur.
+        //public Product(string name)
+        //{
+        //    Name = name ?? throw new ArgumentNullException(nameof(name));
+        //}
+
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+
+        //[ForeignKey("CategoryId")] EF Core direkt görüyor eğer görmeseydi Attribute tanımlayacaktık.
+        public Category Category { get; set; }
+
+        //Aşağıdaki tanımlama "Navigation Property" olarak geçmektedir. Product'dan ProductFeature'a veya Category'e gidebilirim.
+        public ProductFeature ProductFeature { get; set; }
+
+
+    }
+}
---
+++ NLayer.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq.Expressions;
+
+namespace NLayer.Core.Repositories
+{
+    public interface IGenericRepository<T> where T : BaseEntity
+    {
+        //=>GetAll,Where Neden asenkron tanımlamadık aşağıda henüz veritabanına sorgu yapmıyoruz aşağıdaki metot ile veritabanına yapılacak sorguyu oluşturuyoruz. Bu sebeple IQueryable seçtik. ToList dendiğinde veritabanına gidip sorgu yapar.
+        //=>productRepository.where(x=> x.id>5).OrderBy.ToList() - ToList dediğim anda sorgu yapar.
+        //=>EF Core da Update ve Delete için asenkron olan yapısı yoktur.Entity'i zaten EF Core takip ediyor, Update metoduna geldiğinde State'ini Modify yapıyor. Yani uzun süren bir işlem olmadığı için asenkronu yoktur.
+        Task<T> GetByIdAync(int id);
+        IQueryable<T> GetAll(Expression<Func<T, bool>> expression);
+        IQueryable<T> Where(Expression<Func<T,bool>> expression);
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        void Update(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+    }
+}
